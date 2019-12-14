d178b587632b917db6e2b14d229db4b893f62c2f
1sudo <zjmw00@gmail.com>
2019-12-14 00:24:38 -0600

    More login and registration work, needs additional validation

--- venv/Classes/db.py
+++ venv/Classes/db.py
@@ -1,34 +1,88 @@
+import hashlib
+from random import randrange
+from flask_mysqldb import MySQL
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlsoup import SQLSoup
+from var_dump import var_dump
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "person"
+class Account(Base):
+    __tablename__ = "accounts"
 
     id = Column('id', Integer, primary_key=True)
-    username = Column('username', String, unique=True)
+    email = Column('email', String, nullable=False)
+    username = Column('username', String, nullable=False)
+    password = Column('password', String, nullable=False)
+    salt = Column('salt', String, nullable=False)
+    admin_level = Column('admin_level', Integer, nullable=False)
 
 
-engine = create_engine('sqlite:///users.db', echo=True)
-Base.metadata.create_all(bind=engine)
+class String(Base):
+    __tablename__ = "strings"
+
+    id = Column('id', Integer, primary_key=True)
+    account_id = Column('account_id', Integer, nullable=False)
+    stringfile = Column('stringFile', String, nullable=False)
+    index = Column('index', Integer, nullable=False)
+    name = Column('name', String, nullable=False)
+    value = Column('value', String, nullable=False)
+
+
+engine = create_engine('mysql://sredit:123456@localhost:3306/sredit', echo=True)
 Session = sessionmaker(bind=engine)
-
 session = Session()
 
-"""user = User()
-user.id = 0
-user.username = "alice"
 
-session.add(user)
-session.commit()"""
+class Query:
 
-users = session.query(User).all()
+    def __init__(self):
+        print("Query Engine Initialized")
 
-for user in users:
-    print("Username: {}, ID: {}".format(user.username, user.id))
+    def create_account(self, params):
+        db = SQLSoup(engine)
 
+        # Generate a random 32 character number and get it's md5 hash
+        random_num = randrange(100000000000000000000000000000000, 999999999999999999999999999999999, 2)
+        salt = hashlib.md5(str(random_num).encode())
 
+        # Concatenate the users password and salt, then hash it with sha256
+        concat = str(params['password']) + str(salt.hexdigest())
+        pass_hash = hashlib.sha256(concat.encode())
 
-session.close()
+        db.accounts.insert(project_name=params['project_name'], email=params['email'], username=params['username'], password=pass_hash.hexdigest(), salt=salt.hexdigest())
+        db.commit()
+        
+    def get_hash(self, password, salt):
+        concat = str(password) + str(salt)
+        pass_hash = hashlib.sha256(concat.encode())
+        return pass_hash.hexdigest()
+
+    def check_username(self, username):
+        db = SQLSoup(engine)
+        try:
+            result = db.accounts.filter(db.accounts.username==username).one()
+            return "found"
+        except:
+            return "not found"
+
+    def check_email(self, email):
+        db = SQLSoup(engine)
+        try:
+            result = db.accounts.filter(db.accounts.email==email).one()
+            return "found"
+        except:
+            return "not found"
+
+    def get_account(self, username):
+        db = SQLSoup(engine)
+        try:
+            result = db.accounts.filter(db.accounts.username==username).one()
+            return result
+        except:
+            return -1
+
+    def get_stringfiles_by_account_id(self, account_id):
+        return session.query(String).filter_by(stringfile='string/en/aprilfools.stf')
--- venv/app.py
+++ venv/app.py
@@ -3,19 +3,25 @@ from flask_session import Session
 from flask_redis import FlaskRedis
 from Classes.stf_reader import STFReader
 from Classes.session import SRSession
+from Classes.db import Query
+from var_dump import var_dump
+
 
 sesh = SRSession()
 
 app = Flask(__name__)
 
 SESSION_TYPE = 'redis'
+SESSION_PERMANENT = False
 app.config.from_object(__name__)
 Session(app)
+
+qm = Query
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # Check if the user is logged in - if not, redirect to login page
-    loggedin = sesh.get('somekey')
+    loggedin = sesh.get('username')
     if loggedin == 'no':
         return redirect('/login', code=302)
     else:
@@ -34,11 +40,72 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        errors = []
+        params = {}
+        params['username'] = request.form.get('username')
+        params['password'] = request.form.get('password')
+
+        account = qm.get_account("", params['username'])
+
+        if hasattr(account, 'username'):
+            hash = qm.get_hash("", params['password'], account.salt)
+            if account.password == hash:
+                sesh.set('id', account.id)
+                sesh.set('project_name', account.project_name)
+                sesh.set('username', account.username)
+                sesh.set('admin_level', account.admin_level)
+                return redirect('/', code=302)
+            else:
+                errors.append('Password incorrect!')
+                return render_template('login.html', errors=errors)
+        else:
+            errors.append('Username does not exist!')
+            return render_template('login.html', errors=errors)
+
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        errors = []
+        params = {}
+        params['project_name'] = request.form.get('project_name')
+        params['email'] = request.form.get('email')
+        params['username'] = request.form.get('username')
+        params['password'] = request.form.get('password')
+        params['password_confirm'] = request.form.get('password_confirm')
+
+        email_check = qm.check_email("", params['email'])
+        username_check = qm.check_username("", params['username'])
+
+        if params['password'] != params['password_confirm']:
+            errors.append('Passwords do not match!')
+            return render_template('register.html', errors=errors)
+        elif email_check == "found":
+            errors.append('Email address exists!')
+            return render_template('register.html', errors=errors)
+        elif username_check == "found":
+            errors.append('Username exists!')
+            return render_template('register.html', errors=errors)
+        else:
+            qm.create_account("", params)
+            sesh.set('project_name', params['project_name'])
+            sesh.set('username', params['username'])
+            sesh.set('admin_level', 0)
+            return redirect('/', code=302)
+    else:
+        return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    sesh.set('project_name', 'null')
+    sesh.set('username', 'no')
+    sesh.set('admin_level', 0)
+    return redirect('/login', code=302)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
