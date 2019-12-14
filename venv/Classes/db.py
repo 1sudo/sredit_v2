fd3065b83a120b2f419027d67cf90cd5f01ba618
1sudo <zjmw00@gmail.com>
2019-12-14 04:51:32 -0600

    Some STF editor styling

--- venv/Classes/db.py
+++ venv/Classes/db.py
@@ -1,6 +1,5 @@
 import hashlib
 from random import randrange
-from flask_mysqldb import MySQL
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
